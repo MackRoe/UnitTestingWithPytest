1c28ba7cd31a7bd7e356631bb5c3e5b5e0d6e674
MackRoe <elaine.music@students.makeschool.com>
2021-02-23 14:32:30 -0500

    correct pep8 issue in exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,7 +1,6 @@
 import math
 
 T_HALF = 5730
-# What is T_HALF? Unclear variable naming.
 DECAY_CONSTANT = -0.693
 
 
@@ -9,9 +8,13 @@ def get_age_carbon_14_dating(carbon_14_ratio):
     """Returns the estimated age of the sample in year.
     carbon_14_ratio: the percent (0 < percent < 1) of carbon-14
     in the sample conpared to the amount in living
-    tissue (unitless).
+    tissue (unitless)
     """
-    return math.log(carbon_14_ratio) / DECAY_CONSTANT * T_HALF
+    if carbon_14_ratio > 0 and carbon_14_ratio < 1:
+        return math.log(carbon_14_ratio) / DECAY_CONSTANT * T_HALF
+    else:
+        print("Input must be a percentage between 0 and 1")
+        return
 
 # TODO: Write a unit test which feed 0.35 to the function.
 # The result should be '8680.34'. Does the function handle
@@ -20,3 +23,7 @@ def get_age_carbon_14_dating(carbon_14_ratio):
 # Add the necessary logic to make sure the function handles
 # every possible input properly. Then write a unit test against
 # this special case.
+
+
+def test_get_age_carbon14_dating():
+    assert get_age_carbon_14_dating(0.35) == 8680.34
