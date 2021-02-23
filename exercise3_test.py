925403c040550b7324d77d0b9051a7cbbb689677
MackRoe <elaine.music@students.makeschool.com>
2021-02-23 15:40:34 -0500

    add math import to exercise3.py

--- exercise3_test.py
+++ exercise3_test.py
@@ -1,9 +1,10 @@
 # exercise3.py
 # TODO: Write a unit test for calculate_stat() function.
 
+import math
 from exercise3 import calculate_stat
 
 
 def test_calculate_stat(grade_list):
     grade_list = [2.5, 3.0, 3.5, 4.0]
-    assert calculate_stat(grade_list) == 3.25, 0.5590169943749475
+    assert math.isclose(calculate_stat(grade_list) == 3.25, 0.5590169943749475)
